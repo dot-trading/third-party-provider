aa314e67d91e338b2adadedded30746202ec3e26
Hamza SAII <hamza.saii@outlook.com>
2026-05-17 21:44:02 +0200

    fix: property name

--- src/TradingProject.ThirdParty.Application/Common/Models/FearAndGreed.cs
+++ src/TradingProject.ThirdParty.Application/Common/Models/FearAndGreed.cs
@@ -1,4 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace TradingProject.ThirdParty.Application.Common.Models;
 
 public record FearAndGreedResponseDto(List<FearAndGreedDataDto> Data);
-public record FearAndGreedDataDto(int Value, string ValueClassification, long Timestamp);
+public record FearAndGreedDataDto(
+    int Value,
+    [property: JsonPropertyName("value_classification")] string ValueClassification,
+    long Timestamp);
